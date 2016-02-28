976817375033680ac13e5116f38e8e3d5c71aa43
Manu <sefirot_cloud@hotmail.com>
2016-02-28 20:31:15 +0100

    Clase 56: Creando entidades

---
+++ src/BlogBundle/Entity/EntryTag.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * EntryTag
+ *
+ * @ORM\Table(name="entry_tag", indexes={@ORM\Index(name="fk_entry_tag_entries", columns={"entry_id"}), @ORM\Index(name="fk_entry_tag_tags", columns={"tag_id"})})
+ * @ORM\Entity
+ */
+class EntryTag
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var Entry
+     *
+     * @ORM\ManyToOne(targetEntity="Entry")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="entry_id", referencedColumnName="id")
+     * })
+     */
+    private $entry;
+
+    /**
+     * @var Tag
+     *
+     * @ORM\ManyToOne(targetEntity="Tag")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="tag_id", referencedColumnName="id")
+     * })
+     */
+    private $tag;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set entry
+     *
+     * @param \BlogBundle\Entity\Entry $entry
+     *
+     * @return EntryTag
+     */
+    public function setEntry(\BlogBundle\Entity\Entry $entry = null)
+    {
+        $this->entry = $entry;
+
+        return $this;
+    }
+
+    /**
+     * Get entry
+     *
+     * @return \BlogBundle\Entity\Entry
+     */
+    public function getEntry()
+    {
+        return $this->entry;
+    }
+
+    /**
+     * Set tag
+     *
+     * @param \BlogBundle\Entity\Tag $tag
+     *
+     * @return EntryTag
+     */
+    public function setTag(\BlogBundle\Entity\Tag $tag = null)
+    {
+        $this->tag = $tag;
+
+        return $this;
+    }
+
+    /**
+     * Get tag
+     *
+     * @return \BlogBundle\Entity\Tag
+     */
+    public function getTag()
+    {
+        return $this->tag;
+    }
+}
---
+++ src/BlogBundle/Entity/Tag.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tag
+ *
+ * @ORM\Table(name="tags")
+ * @ORM\Entity
+ */
+class Tag
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", length=65535, nullable=true)
+     */
+    private $description;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Tag
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Tag
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
