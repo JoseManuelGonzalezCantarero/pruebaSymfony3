01616ec474b5cfce14db9abf72aae062d2b75987
Manu <sefirot_cloud@hotmail.com>
2016-02-28 20:45:42 +0100

    Clase 58: Relaciones Many-To-One

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -8,10 +8,23 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/blog", name="blog")
      */
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+        $entry_repo = $em->getRepository("BlogBundle:Entry");
+        $entries = $entry_repo->findAll();
+
+        foreach($entries as $entry)
+        {
+            echo $entry->getTitle().'<br>';
+            echo $entry->getCategory()->getName().'<br>';
+            echo $entry->getUser()->getName().'<br><hr>';
+        }
+
+        die();
+
         return $this->render('BlogBundle:Default:index.html.twig');
     }
 }
