4e09f5b60623cbece341f724b2d20d2dddbd077e
manu <sefirot_cloud@hotmail.com>
2016-03-02 10:40:18 +0100

    Clase 65: Formulario de registro

--- src/BlogBundle/Controller/UserController.php
+++ src/BlogBundle/Controller/UserController.php
@@ -2,6 +2,8 @@
 
 namespace BlogBundle\Controller;
 
+use BlogBundle\Entity\User;
+use BlogBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,9 +19,43 @@ class UserController extends Controller
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUserName = $authenticationUtils->getLastUsername();
 
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isValid())
+        {
+            $user = new User();
+            $user->setName($form->get("name")->getData());
+            $user->setSurname($form->get("surname")->getData());
+            $user->setEmail($form->get("email")->getData());
+            $user->setPassword($form->get("password")->getData());
+            $user->setRole('ROLE_USER');
+            $user->setImage(null);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $flush = $em->flush();
+            if($flush == null)
+            {
+                $status = "El usuario se ha registrado correctamente";
+            }
+            else
+            {
+                $status = "No te has registrado correctamente";
+            }
+
+        }
+        else
+        {
+            $status = "No te has registrado correctamente";
+        }
+
         return $this->render('BlogBundle:user:login.html.twig', array(
             'error' => $error,
-            'lastUserName' => $lastUserName
+            'lastUserName' => $lastUserName,
+            'form' => $form->createView(),
         ));
     }
 
